8bdf629d52b58a4d9ec7e4b481eaad3318d950ff
ISHWARYA <ishwaryathendral@gmail.com>
2022-10-10 18:17:03 +0530

    coloured bullets

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -15,11 +15,11 @@ export default function Main() {
         <main>
             <h1 className="main--title">Fun facts about React</h1>
             <ul className="main--facts">
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100K stars on GitHub</li>
-                <li>Is maintained by Facebook</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
+                <li><span>Was first released in 2013</span></li>
+                <li><span>Was originally created by Jordan Walke</span></li>
+                <li><span>Has well over 100K stars on GitHub</span></li>
+                <li><span>Is maintained by Facebook</span></li>
+                <li><span>Powers thousands of enterprise apps, including mobile apps</span></li>
             </ul>
         </main>
     )
